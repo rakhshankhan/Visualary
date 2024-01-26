cb7b943ccb79949562166ff058fcdae89c6d7d3d
Rakhshan Khan <mkha45@uwo.ca>
2024-01-26 01:07:52 -0500

    initial commit

---
+++ server/src/main/java/com/rakhshankhan/visualary/VisualaryApplication.java
@@ -0,0 +1,13 @@
+package com.rakhshankhan.visualary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualaryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/rakhshankhan/visualary/VisualaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakhshankhan.visualary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
